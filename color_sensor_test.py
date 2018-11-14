04b3e91898f1804122c7c385f0948aa74e42977e
Jimmy Nguyen <nguyenjim6@gmail.com>
2018-11-14 00:47:05 -0600

    stylized code and left some notes TODO: need to go over notes w/ Kyle

--- color_sensor_test.py
+++ color_sensor_test.py
@@ -1,35 +1,55 @@
 import smbus
 import time
+
 bus = smbus.SMBus(1)
 bus.write_byte(0x29,0x80|0x12)
 ver = bus.read_byte(0x29)
 print hex(ver)
-if ver == 0x44:
-	print "device found!/n"
-	bus.write_byte(0x29, 0x80|0x00)
-	bus.write_byte(0x29, 0x01|0x02)
-	bus.write_byte(0x29, 0x80|0x14)
-	prev_red = 0
-	prev_blue = 0
-	count = 0
-	data = bus.read_i2c_block_data(0x29, 0)
+
+def read():
+    data = bus.read_i2c_block_data(0x29, 0)
+    clear = data[1] << 8 | data[0]
+    red = data[3] << 8 | data[2]
+    green = data[5] << 8 | data[4]
+    blue = data[7] << 8 | data[6]
+
+    if (red - prev_red) >= (blue - prev_blue) \
+      and (red - prev_red) > 1000 \
+      and count > 1:
+        print "red bag detected!\n"
+        #time.sleep(0.15)
+
+    elif (blue - prev_blue) >= 800 \
+      and (blue - prev_blue) > (red - prev_red) \
+      and count > 0:
+        print "blue bag detected!\n"
+        #time.sleep(0.15)
+
+    #crgb = "c: %s, R: %s, G: %s, B: %s\n" % (clear, red, green, blue)
+    #print crgb
+    prev_red = red
+    prev_blue = blue
+    count += 1
+    #time.sleep(1)
+
+def main():
+    global bus, ver
+
+    if ver == 0x44:
+        print "device found!/n"
+        bus.write_byte(0x29, 0x80|0x00)
+        bus.write_byte(0x29, 0x01|0x02)
+        bus.write_byte(0x29, 0x80|0x14)
+        prev_red = 0
+        prev_blue = 0
+        count = 0
+        data = bus.read_i2c_block_data(0x29, 0)
+
         while True:
-		data = bus.read_i2c_block_data(0x29, 0)
-		clear = data[1] << 8 | data[0]
-		red = data[3] << 8 | data[2]
-		green = data[5] << 8 | data[4]
-		blue = data[7] << 8 | data[6]
-		if (red - prev_red) >= (blue - prev_blue) and (red- prev_red) > 1000 and count > 1:
-			print "red bag detected!\n"
-			#time.sleep(0.15)
-		elif (blue - prev_blue) >= 800 and (blue - prev_blue) > (red - prev_red) and count > 0:
-			print "blue bag detected!\n"
-			#time.sleep(0.15)
-		#crgb = "c: %s, R: %s, G: %s, B: %s\n" % (clear, red, green, blue)
-		#print crgb
-		prev_red = red
-		prev_blue = blue
-		count += 1
-		#time.sleep(1)
-else:
-	print "Device not found!\n"
+            read()
+
+    else:
+            print "Device not found!\n"
+
+if __name__ == "__main__":
+    main()
--- opencv_test.py
+++ opencv_test.py
@@ -7,61 +7,77 @@ prev_red = 5
 blue_count = 0
 prev_blue = 5
 
-while(True):
-    # Capture frame-by-frame
-    red_count = 0
-    blue_count = 0
-    ret,frame = cap.read()
-    frame = frame[0:720,85:525]
+def main():
+    global cap, red_count, prev_red, blue_count, prev_blue
 
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    while(True):
+        # Capture frame-by-frame
+        red_count = 0
+        blue_count = 0
+        ret,frame = cap.read()
+        frame = frame[0:720, 85:525] #NOTE: does this change?
 
-    red_lower = np.array([135,64,43],np.uint8)
-    red_upper = np.array([180,255,255],np.uint8)
+        hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-    blue_lower = np.array([91,74,65],np.uint8)
-    blue_upper = np.array([124,255,255],np.uint8)
+        # NOTE: do these red/blue values ever change?
+        # set red min/max color values
+        red_lower = np.array([135, 64, 43], np.uint8)
+        red_upper = np.array([180, 255, 255], np.uint8)
+        red = cv2.inRange(hsv, red_lower, red_upper)
 
-    red = cv2.inRange(hsv, red_lower, red_upper)
-    blue = cv2.inRange(hsv, blue_lower, blue_upper)
+        # set blue min/max color values
+        blue_lower = np.array([91, 74, 65], np.uint8)
+        blue_upper = np.array([124, 255, 255], np.uint8)
+        blue = cv2.inRange(hsv, blue_lower, blue_upper)
 
-    kernal = np.ones((5,5),"uint8")
-    red1 = cv2.dilate(red, kernal)
-    res = cv2.bitwise_and(frame, frame, mask = red)
+        # NOTE: does kernal or red1/blue1 or res/res1 ever change?
+        kernal = np.ones((5,5), "uint8")
+        red1 = cv2.dilate(red, kernal)
+        res = cv2.bitwise_and(frame, frame, mask = red)
 
-    blue1 = cv2.dilate(blue, kernal)
-    res1 = cv2.bitwise_and(frame, frame, mask = blue)
+        blue1 = cv2.dilate(blue, kernal)
+        res1 = cv2.bitwise_and(frame, frame, mask = blue)
 
-    contours,hierarchy = cv2.findContours(red1,cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-   # contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-    cv2.drawContours(frame, contours, -1, (0,0,255),2)
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area>700):
-           # x,y,w,h = cv2.boundingRect(contour)
-           #cv2.drawContours(frame,contours,-1,(0,0,255),2)
-           # cv2.putText(frame,"RED color",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255))
-           red_count += 1
+        # NOTE: rename contours to bags?
+        contours, hierarchy = cv2.findContours(red1,
+                                               cv2.RETR_TREE,
+                                               cv2.CHAIN_APPROX_NONE)
+       # contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+        cv2.drawContours(frame, contours, -1, (0,0,255), 2)
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            # TODO: these magic numbers should be variablized
+            # something like MIN_BAG_AREA
+            if(area > 700):
+               # x,y,w,h = cv2.boundingRect(contour)
+               # cv2.drawContours(frame,contours,-1,(0,0,255),2)
+               # cv2.putText(frame,"RED color",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255))
+               red_count += 1
 
-    contours,hierarchy = cv2.findContours(blue1,cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(frame, contours, -1, (255,0,0), 2)
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area>700):
-            #cv2.drawContours(frame,contours,-1,(255,0,0),2)
-           # cv2.putText(frame,"BLUE color",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,0,0))
-            blue_count += 1
+        contours, hierarchy = cv2.findContours(blue1,
+                                               cv2.RETR_TREE,
+                                               cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(frame, contours, -1, (255,0,0), 2)
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if(area > 700):
+                #cv2.drawContours(frame,contours,-1,(255,0,0),2)
+               # cv2.putText(frame,"BLUE color",(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,0,0))
+                blue_count += 1
 
-    if blue_count != prev_blue or red_count != prev_red:
-        print("Red Count: " + str(red_count))
-        print("Blue Count: " + str(blue_count))
-        prev_red = red_count
-        prev_blue = blue_count
+        # display stuff
+        if blue_count != prev_blue or red_count != prev_red:
+            print("Red Count: " + str(red_count))
+            print("Blue Count: " + str(blue_count))
+            prev_red = red_count
+            prev_blue = blue_count
 
+        # GUI window
+        cv2.imshow("Color Tracking",frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break
 
-    cv2.imshow("Color Tracking",frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        cap.release()
-        cv2.destroyAllWindows()
-        break
-
+if __name__ == "__main__":
+    main()

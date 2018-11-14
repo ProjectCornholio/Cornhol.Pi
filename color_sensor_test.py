ddaef205785b9bdcaef11ac5383723440ccd3990
Jimmy Nguyen <nguyenjim6@gmail.com>
2018-11-14 00:50:44 -0600

    adding a couple generic comments

--- color_sensor_test.py
+++ color_sensor_test.py
@@ -7,18 +7,21 @@ ver = bus.read_byte(0x29)
 print hex(ver)
 
 def read():
+    # read register from color sensor
     data = bus.read_i2c_block_data(0x29, 0)
     clear = data[1] << 8 | data[0]
     red = data[3] << 8 | data[2]
     green = data[5] << 8 | data[4]
     blue = data[7] << 8 | data[6]
 
+    # detected red bag
     if (red - prev_red) >= (blue - prev_blue) \
       and (red - prev_red) > 1000 \
       and count > 1:
         print "red bag detected!\n"
         #time.sleep(0.15)
 
+    # detected blue bag
     elif (blue - prev_blue) >= 800 \
       and (blue - prev_blue) > (red - prev_red) \
       and count > 0:

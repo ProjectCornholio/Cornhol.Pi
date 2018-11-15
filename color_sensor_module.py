cdec361f0a848e2a367d2235e9a5472bb751357c
KMarcrum <kmarcrum@gmail.com>
2018-11-15 00:36:55 -0500

    turning colorsensor into a class

--- color_sensor_module.py
+++ color_sensor_module.py
@@ -1,12 +1,57 @@
 import smbus
 import time
 
-bus = smbus.SMBus(1)
-bus.write_byte(0x29,0x80|0x12)
-ver = bus.read_byte(0x29)
-print hex(ver)
 
-def read():
+class ColorSensor():
+    def __init__(self):
+        self.__bus = smbus.SMBus(1)
+        self.__bus.write_byte(0x29,0x80|0x12)
+        ver = self.__bus.read_byte(0x29)
+
+        if ver == 0x44:
+            print "device found!\n"
+            self.__bus.write_byte(0x29, 0x80|0x00) # enable register
+            self.__bus.write_byte(0x29, 0x01|0x02) # (power on | rgb sensor en)
+            self.__bus.write_byte(0x29, 0x80|0x14) # read reg starts with 0x14
+            self.__prev_red = 0
+            self.__prev_blue = 0
+            self.__count = 0
+            data = self.__bus.read_i2c_block_data(0x29, 0)
+
+        else:
+                print "Device not found!\n"
+        
+    def read(self):
+        # read register from color sensor
+        data = self.__bus.read_i2c_block_data(0x29, 0)
+        clear = data[1] << 8 | data[0]
+        red = data[3] << 8 | data[2]
+        green = data[5] << 8 | data[4]
+        blue = data[7] << 8 | data[6]
+
+        # detected red bag
+        if (red - self.__prev_red) > (blue - self.__prev_blue) \
+          and (red - self.__prev_red) >= 1000 \
+          and self.__count > 1:
+            print "red bag detected!\n"
+            #time.sleep(0.15)
+
+        # detected blue bag
+        elif (blue - self.__prev_blue) > (red - self.__prev_red) \
+          and (blue - self.__prev_blue) >= 800 \
+          and self.__count > 0:
+            print "blue bag detected!\n"
+            #time.sleep(0.15)
+
+        #crgb = "c: %s, R: %s, G: %s, B: %s\n" % (clear, red, green, blue)
+        #print crgb
+        self.__prev_red = red
+        self.__prev_blue = blue
+        self.__count += 1
+        #time.sleep(1)
+        
+
+def read(bus):
     # read register from color sensor
     data = bus.read_i2c_block_data(0x29, 0)
     clear = data[1] << 8 | data[0]
@@ -18,6 +63,7 @@ def read():
     if (red - prev_red) >= (blue - prev_blue) \
       and (red - prev_red) > 1000 \
       and count > 1:
+        return (1,0)
         print "red bag detected!\n"
         #time.sleep(0.15)
 
@@ -25,6 +71,7 @@ def read():
     elif (blue - prev_blue) >= 800 \
       and (blue - prev_blue) > (red - prev_red) \
       and count > 0:
+        return (0,1)
         print "blue bag detected!\n"
         #time.sleep(0.15)
 
@@ -36,7 +83,10 @@ def read():
     #time.sleep(1)
 
 def main():
-    global bus, ver
+    bus = smbus.SMBus(1)
+    bus.write_byte(0x29,0x80|0x12)
+    ver = bus.read_byte(0x29)
+    print hex(ver)
 
     if ver == 0x44:
         print "device found!/n"
@@ -49,10 +99,12 @@ def main():
         data = bus.read_i2c_block_data(0x29, 0)
 
         while True:
-            read()
+            read(bus)
 
     else:
             print "Device not found!\n"
 
 if __name__ == "__main__":
-    main()
+    sensor = ColorSensor()
+    while True:
+        sensor.read()

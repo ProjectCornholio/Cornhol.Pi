6b9d23faf0cb2c8f301a2ce02a5a6ebfe01dd232
KMarcrum <kmarcrum@gmail.com>
2018-11-15 01:52:15 -0500

    create camera class for opencv module

--- opencv_module.py
+++ opencv_module.py
@@ -1,6 +1,58 @@
 import numpy as np
 import cv2
 
+class Camera():
+    def __init__(self):
+        self.__cap = cv2.VideoCapture(0)
+        self.__red_count = 0
+        self.__prev_red = 5
+        self.__blue_count = 0
+        self.__prev_blue = 5
+
+        self.__red_lower = np.array([135, 64, 43], np.uint8)
+        self.__red_upper = np.array([180, 255, 255], np.uint8)
+        self.__blue_lower = np.array([91, 74, 65], np.uint8)
+        self.__blue_upper = np.array([124, 255, 255], np.uint8)
+        self.__kernal = np.ones((5,5), "uint8")
+        self.__min_bag_area = 700
+
+    def read(self):
+        # Capture frame-by-frame
+        red_count = 0
+        blue_count = 0
+        ret, frame = self.__cap.read()
+        frame = frame[0:720, 85:525]
+
+        # set color values in hsv spectrum and dilate
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        red = cv2.inRange(hsv, self.__red_lower, self.__red_upper)
+        blue = cv2.inRange(hsv, self.__blue_lower, self.__blue_upper)
+        red_dilate = cv2.dilate(red, self.__kernal)
+        blue_dilate = cv2.dilate(blue, self.__kernal)
+
+        contours, hierarchy = cv2.findContours(red_dilate,
+                                               cv2.RETR_TREE,
+                                               cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(frame, contours, -1, (0,0,255), 2)
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if(area > self.__min_bag_area):
+               red_count += 1
+
+        contours, hierarchy = cv2.findContours(blue_dilate,
+                                               cv2.RETR_TREE,
+                                               cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(frame, contours, -1, (255,0,0), 2)
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if(area > self.__min_bag_area):
+                blue_count += 1
+
+        return red_count, blue_count
+
+    def close(self):
+        self.__cap.release()
+        cv2.destroyAllWindows()
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -8,38 +60,33 @@ def main():
     prev_red = 5
     blue_count = 0
     prev_blue = 5
+
+    red_lower = np.array([135, 64, 43], np.uint8)
+    red_upper = np.array([180, 255, 255], np.uint8)
+    blue_lower = np.array([91, 74, 65], np.uint8)
+    blue_upper = np.array([124, 255, 255], np.uint8)
+    kernal = np.ones((5,5), "uint8")
     min_bag_area = 700
 
     while(True):
         # Capture frame-by-frame
         red_count = 0
         blue_count = 0
-        ret,frame = cap.read()
-        frame = frame[0:720, 85:525] #NOTE: does this change?
+        ret, frame = cap.read()
+        frame = frame[0:720, 85:525]
 
+        # set color values in hsv spectrum and dilate
         hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-
-        # NOTE: do these red/blue values ever change? red/blue_lower and red/blue_upper don't change, but red and blue do cause it takes in each new frame
-        # set red min/max color values
-        red_lower = np.array([135, 64, 43], np.uint8)
-        red_upper = np.array([180, 255, 255], np.uint8)
         red = cv2.inRange(hsv, red_lower, red_upper)
-
-        # set blue min/max color values
-        blue_lower = np.array([91, 74, 65], np.uint8)
-        blue_upper = np.array([124, 255, 255], np.uint8)
         blue = cv2.inRange(hsv, blue_lower, blue_upper)
+        red_dilate = cv2.dilate(red, kernal)
+        blue_dilate = cv2.dilate(blue, kernal)
 
-        # NOTE: does kernal or red1/blue1 or res/res1 ever change? Kernal doesn't but red/blue1 and res/res1 does because it changes every frame
-        kernal = np.ones((5,5), "uint8")
-        red1 = cv2.dilate(red, kernal)
-        res = cv2.bitwise_and(frame, frame, mask = red)
-
-        blue1 = cv2.dilate(blue, kernal)
-        res1 = cv2.bitwise_and(frame, frame, mask = blue)
+        res = cv2.bitwise_and(frame, frame, mask=red)
+        res1 = cv2.bitwise_and(frame, frame, mask=blue)
 
         # NOTE: rename contours to bags? not actually just bags, it's all countours being found
-        contours, hierarchy = cv2.findContours(red1,
+        contours, hierarchy = cv2.findContours(red_dilate,
                                                cv2.RETR_TREE,
                                                cv2.CHAIN_APPROX_NONE)
         cv2.drawContours(frame, contours, -1, (0,0,255), 2)
@@ -48,7 +95,7 @@ def main():
             if(area > min_bag_area):
                red_count += 1
 
-        contours, hierarchy = cv2.findContours(blue1,
+        contours, hierarchy = cv2.findContours(blue_dilate,
                                                cv2.RETR_TREE,
                                                cv2.CHAIN_APPROX_NONE)
         cv2.drawContours(frame, contours, -1, (255,0,0), 2)
@@ -72,4 +119,15 @@ def main():
             break
 
 if __name__ == "__main__":
-    main()
+    cam = Camera()
+    red_cnt = 0
+    blue_cnt = 0
+    prev_red = red_cnt
+    prev_blue = blue_cnt
+
+    while True:
+        red_cnt, blue_cnt = cam.read()
+        if prev_red != red_cnt or prev_blue != blue_cnt:
+            print "RED COUNT: %s\t\nBLUE COUNT:%s\t\n" % (str(red_cnt), str(blue_cnt))
+            prev_red = red_cnt
+            prev_blue = blue_cnt

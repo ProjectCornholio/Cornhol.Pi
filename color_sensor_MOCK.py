bc03d8cd189ce7bcf48ff3eb0bf11882294c7045
Jimmy Nguyen <jtn136@msstate.edu>
2018-11-15 23:32:47 -0600

    fixing the mocks

--- color_sensor_MOCK.py
+++ color_sensor_MOCK.py
@@ -1,2 +1,8 @@
-def read():
-    return 1, 0
+class ColorSensor():
+    def __init__(self):
+        print "Initializing MOCK Color Sensor..."
+        print "Done"
+        
+    def read(self):
+        ret_val = (0,1)
+        return ret_val
--- opencv_MOCK.py
+++ opencv_MOCK.py
@@ -1,2 +1,13 @@
-def read():
-    return 3, 4
+class Camera():
+    def __init__(self):
+        print "Initializing MOCK Camera..."
+        print "Done"
+
+    def read(self, gui=False):
+        red_count = 2
+        blue_count = 1
+        return red_count, blue_count
+
+    def close(self):
+        return
+

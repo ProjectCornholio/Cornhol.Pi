67f17458dd9d4726181986d0a4d141306cc3fc12
Jimmy Nguyen <jtn136@msstate.edu>
2018-11-27 14:24:28 -0600

    changing send message to phone

--- cornholio_main.py
+++ cornholio_main.py
@@ -91,7 +91,8 @@ def tx_thread(phone, send_rate=1):
         if curr_time - prev_time > send_rate:
             msg = "Board:\tRED: %s\n\tBLUE: %s\n" % (BOARD_RED, BOARD_BLUE)
             msg += "Hole:\tRED: %s\n\tBLUE: %s\n" % (HOLE_RED, HOLE_BLUE)
-            phone.tx(msg)
+            phone.tx("%s,%s,%s,%s" % (BOARD_RED, HOLE_RED,
+                                      BOARD_BLUE, HOLE_BLUE))
             print msg
             prev_time = time.time()
     NUM_THREADS -= 1

fb0017d09385c6b6ae1442e733ed51dfd0b1e1cb
Jimmy Nguyen <jtn136@msstate.edu>
2018-11-27 14:51:48 -0600

    doot doot threshold

--- opencv_module.py
+++ opencv_module.py
@@ -41,7 +41,7 @@ class Camera():
         contours, hierarchy = cv2.findContours(red_dilate,
                                                cv2.RETR_TREE,
                                                cv2.CHAIN_APPROX_NONE)
-        cv2.drawContours(frame, contours, -1, (0,0,255), 2)
+        cv2.drawContours(frame, contours, -1, (0,255,0), 2)
         for pic, contour in enumerate(contours):
             area = cv2.contourArea(contour)
             if(area > self.__min_bag_area):
@@ -50,7 +50,7 @@ class Camera():
         contours, hierarchy = cv2.findContours(blue_dilate,
                                                cv2.RETR_TREE,
                                                cv2.CHAIN_APPROX_NONE)
-        cv2.drawContours(frame, contours, -1, (255,0,0), 2)
+        cv2.drawContours(frame, contours, -1, (0,165,255), 2)
         for pic, contour in enumerate(contours):
             area = cv2.contourArea(contour)
             if(area > self.__min_bag_area):
